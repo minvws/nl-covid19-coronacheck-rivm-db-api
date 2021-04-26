189dab1116fa3a2b16649e1cc6dabaf665f943d6
Rafael Dulfer <rafael.dulfer@gmail.com>
2021-04-26 14:02:16 +0200

    Create unimplemented functions for dealing with the db

---
+++ event_provider/database.py
@@ -0,0 +1,9 @@
+"""Functions to interface with the DB"""
+
+def check_information(id_hash):
+    """Check the DB if information for a certain id_hash exists"""
+    raise NotImplementedError
+
+def get_events(id_hash):
+    """Get all events belonging to a certain id_hash"""
+    raise NotImplementedError
