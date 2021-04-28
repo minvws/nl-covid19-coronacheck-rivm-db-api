78494e17645d3e49854a8ba2c3807c58fe1f3eaf
Rafael Dulfer <rafael.dulfer@gmail.com>
2021-04-28 15:47:32 +0200

    Get started on tests

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+from event_provider import create_app
+
+
+@pytest.fixture(scope="session")
+def app():
+    app = create_app()
+    yield app
+
+
+@pytest.fixture(scope="session")
+def context(app):
+    context = app.app_context()
+    context.push()
+    yield context
+
+
+@pytest.fixture(scope="session")
+def client(app):
+    yield app.test_client()
---
+++ tests/test_decrypt.py
@@ -0,0 +1,40 @@
+from event_provider.decrypt import rawkey_from_file, decrypt
+from event_provider.config import config
+
+import nacl.secret
+import nacl.utils
+import nacl.hash
+from nacl.encoding import Base64Encoder
+import pytest
+import json
+
+@pytest.fixture
+def rnonce():
+    nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
+    return nonce
+
+@pytest.fixture
+def keyfile():
+    return config['DEFAULT']['decrypt_bsn_key']
+
+def encrypt_payload(data, nonce, keyfile):
+    keyid, key = rawkey_from_file(keyfile)
+    box = nacl.secret.SecretBox(key.encode(), encoder=Base64Encoder)
+    enc_data = box.encrypt(data.encode(), nonce, encoder=Base64Encoder)
+    nonce = enc_data.nonce.decode()
+    ctext = enc_data.ciphertext.decode()
+    payload = {
+        'nonce': nonce,
+        'ctext': ctext,
+        'keyid': keyid
+    }
+    return payload
+
+def test_decryption(rnonce, keyfile):
+    data = {
+        'test': 'test'
+    }
+    encrypted = encrypt_payload(json.dumps(data), rnonce, keyfile)
+    _, key = rawkey_from_file(keyfile)
+    decrypted = decrypt(encrypted['ctext'], encrypted['nonce'], key)
+    assert json.loads(decrypted) == data
