0348e994a1253d8fb35b2ab7dd86b17d120eb60c
Rafael Dulfer <rafael.dulfer@gmail.com>
2021-04-30 11:30:53 +0200

    Make all the decryption work

--- event_provider/decrypt.py
+++ event_provider/decrypt.py
@@ -1,8 +1,13 @@
 """Decryptor functions"""
+import hashlib
+import hmac
 import nacl.secret
+import nacl.public
 import nacl.utils
-from nacl.encoding import Base64Encoder
-from flask import current_app, g
+from nacl.encoding import HexEncoder, Base64Encoder
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+from flask import current_app
 
 
 class MismatchKeyIdError(Exception):
@@ -11,37 +16,64 @@ class MismatchKeyIdError(Exception):
 class Decryptor: #pylint: disable=too-few-public-methods
     """Decryptor class which simply holds data written from disk"""
     def __init__(self):
-        keyfile = current_app.config["DEFAULT"]["decrypt_bsn_key"]
-        key = rawkey_from_file(keyfile)
-        self.bsn_keydata = key
+        keyfile = current_app.config["DEFAULT"]["decrypt_bsn_key_our_priv"]
+        privkey = rawkey_from_file(keyfile)
+        keyfile = current_app.config["DEFAULT"]["decrypt_bsn_key_vws_pub"]
+        pubkey = rawkey_from_file(keyfile)
+        keyfile = current_app.config["DEFAULT"]["hash_bsn_key"]
+        hashkey = rawkey_from_file(keyfile)
+        self.bsn_keydata = {
+            'privkey': privkey,
+            'pubkey': pubkey,
+            'hashkey': hashkey,
+        }
         keyfile = current_app.config["DEFAULT"]["decrypt_payload_key"]
         key = rawkey_from_file(keyfile)
         self.payload_keydata = key
 
 def get_decryptor():
     """Add decryptor to globals if it's not there yet"""
-    if not hasattr(g, "decryptor"):
-        g.decryptor = Decryptor()
-    return g.decryptor
+    if not "decryptor" in current_app.config:
+        current_app.config["decryptor"] = Decryptor()
+    return current_app.config["decryptor"]
 
 def decrypt_bsn(bsn, nonce):
     """Decrypt BSN data"""
     decryptor = get_decryptor()
-    key = decryptor.bsn_keydata
-    return decrypt(bsn, nonce, key)
+    privkey = decryptor.bsn_keydata['privkey']
+    pubkey = decryptor.bsn_keydata['pubkey']
+    return decrypt_libsodium(bsn, nonce, privkey, pubkey)
 
-def decrypt_payload(payload, nonce):
+def decrypt_libsodium(data, nonce, privkey, pubkey):
+    """Generic decrypt function"""
+    privkey = nacl.public.PrivateKey(privkey, Base64Encoder)
+    pubkey = nacl.public.PublicKey(pubkey, Base64Encoder)
+    box = nacl.public.Box(privkey, pubkey)
+    nonce = HexEncoder.decode(nonce)
+    decrypted = box.decrypt(data, nonce, encoder=HexEncoder).decode()
+    return decrypted
+
+def decrypt_payload(payload, iv):
     """Decrypt Payload data"""
     decryptor = get_decryptor()
     key = decryptor.payload_keydata
-    return decrypt(payload, nonce, key)
+    enc = HexEncoder.decode(payload)
+    return decrypt_aes(enc, key, iv)
 
-def decrypt(data, nonce, key):
-    """Generic decrypt function"""
-    box = nacl.secret.SecretBox(key.encode(), encoder=Base64Encoder)
-    nonce = Base64Encoder.decode(nonce)
-    decrypted = box.decrypt(data, nonce, encoder=Base64Encoder).decode()
-    return decrypted
+def decrypt_aes(enc, key, iv):
+    """Decrypt AES encrypted data"""
+    iv = HexEncoder.decode(iv)
+    cipher = Cipher(algorithms.AES(bytes(key, "utf-8")), modes.CBC(iv))
+    decryptor = cipher.decryptor()
+    dec = decryptor.update(enc) + decryptor.finalize()
+    return dec.decode("utf-8").strip()
+
+def hash_bsn(bsn):
+    """Hash a bsn"""
+    decryptor = get_decryptor()
+    key = decryptor.bsn_keydata['hashkey']
+    hma = hmac.new(bytes(key, "utf-8"), bsn.encode(), hashlib.sha256)
+    return hma.hexdigest()
 
 def rawkey_from_file(keyfile):
     """Get rawkey from file"""
