fe7857d1e0fcf84676201fece764cfc831c5108f
Rafael Dulfer <rafael.dulfer@gmail.com>
2021-05-10 12:57:00 +0200

    Create a full test that checks everything

--- tests/itests/test_full.py
+++ tests/itests/test_full.py
@@ -1,10 +1,22 @@
-import pytest
 import random
 import json
+from uuid import uuid4
 
-from flask import current_app
-from .test_decrypt import rnonce, riv, bob_keys, alice_keys, encrypt_libsodium, encrypt_aes, rstring, pad
+import pytest
+from nacl.encoding import HexEncoder, Base64Encoder
+from tests.utests.test_decrypt import encrypt_libsodium, encrypt_aes, rstring, pad
 from event_provider.decrypt import hash_bsn, get_decryptor
+from .test_database import get_log
+
+CHECK_PATH='/v1/check-bsn'
+EVENT_PATH='/v1/vaccinaties'
+
+@pytest.fixture(autouse=True)
+def mock_database(mocker, backend_db):
+    mocker.patch('event_provider.database.psycopg2.connect', return_value=backend_db)
+
+def gen_bsn():
+    return ''.join(random.choice("0123456789") for x in range(9))
 
 def prepare_id_string_hash(bsn):
     string = bsn + "-Test-Testerson-01"
@@ -27,6 +39,21 @@ def get_payload():
         "is": "ignored"
     }
 
+def compare_payloads(new, orig):
+    assert new['vaccinsoort'] == orig['Vaccinsoort']
+    assert new['vaccinmerknaam'] == orig['Vaccinmerknaam']
+    assert new['productnaam'] == orig['Productnaam']
+    assert new['leverancier'] == orig['Leverancier']
+    assert new['batchnummer'] == orig['Batchnummer']
+    assert new['vaccinatiedatum'] == orig['Vaccinatiedatum']
+    assert new['uitvoerende'] == orig['Uitvoerende']
+    assert new['vaccinatieland'] == orig['Vaccinatieland']
+    assert new['vaccinatiestatus'] == orig['Vaccinatiestatus']
+    assert new['ouderDan16'] == orig['Ouderdan16']
+    assert new['hpkCode'] == orig['HPK-code']
+    assert "other" not in new
+    assert "is" not in new
+
 def test_full(rnonce, riv, bob_keys, alice_keys, client, context, backend_db):
     bsn_priv = bob_keys['privkey']
     bsn_pub = alice_keys['pubkey']
@@ -35,17 +62,60 @@ def test_full(rnonce, riv, bob_keys, alice_keys, client, context, backend_db):
     with context:
         decryptor = get_decryptor()
         decryptor.bsn_keydata = {
-            "privkey": bsn_priv,
-            "pubkey": bsn_pub,
+            "privkey": Base64Encoder.encode(bytes(bsn_priv)),
+            "pubkey": Base64Encoder.encode(bytes(bsn_pub)),
             "hashkey": bsn_hash
         }
         decryptor.payload_keydata = payload_key
-        bsn = ''.join(random.choice("0123456789") for x in range(9))
+        bsn = gen_bsn()
         id_hash = prepare_id_string_hash(bsn)
         hashed_bsn = hash_bsn(bsn)
         payload = get_payload()
         enc_payload = encrypt_aes(bytes(pad(json.dumps(payload)), "utf-8"), bytes(payload_key, 'utf-8'), riv)
-        enc_bsn = encrypt_libsodium(json.dumps(bsn), rnonce, alice_keys['privkey'], bob_keys['pubkey'])
+        enc_payload = HexEncoder.encode(enc_payload)
+        enc_payload = str(enc_payload, "utf-8")
+        iv = HexEncoder.encode(riv)
+        iv = str(iv, "utf-8")
+        enc_bsn = encrypt_libsodium(bsn, rnonce, alice_keys['privkey'], bob_keys['pubkey'])['ctext']
         with backend_db.cursor() as cur:
             sql = "INSERT INTO vaccinatie_event (bsn_external, bsn_internal, payload, iv, version_cims, version_vcbe) VALUES (%s, %s, %s, %s, %s, %s);"
-            cur.execute(sql, [id_hash, hashed_bsn, enc_payload, riv, "test", "test"])
+            cur.execute(sql, [id_hash, hashed_bsn, enc_payload, iv, "test", "test"])
+        req_body = {'hashedBsn': id_hash}
+        pre = len(get_log(backend_db))
+        response = client.post(CHECK_PATH, json=req_body)
+        assert response.status_code == 200
+        assert response.json["exists"] is True
+        post = len(get_log(backend_db))
+        assert post > pre
+        pre = post
+        req_body = {'hashedBsn': 'fake_hash'}
+        response = client.post(CHECK_PATH, json=req_body)
+        assert response.status_code == 200
+        assert response.json["exists"] is False
+        post = len(get_log(backend_db))
+        assert post > pre
+        pre = post
+        nonce = HexEncoder.encode(rnonce)
+        nonce = str(nonce, "utf-8")
+        req_body = {
+            'hashedBsn': hashed_bsn,
+            'encryptedBsn': enc_bsn,
+            'nonce': nonce,
+        }
+        response = client.post(EVENT_PATH, json=req_body)
+        assert response.status_code == 200
+        assert response.json
+        assert len(response.json) > 0
+        data = response.json
+        compare_payloads(data[0], payload)
+        post = len(get_log(backend_db))
+        assert post > pre
+        pre = post
+        bsn = gen_bsn()
+        enc_bsn = encrypt_libsodium(json.dumps(bsn), rnonce, alice_keys['privkey'], bob_keys['pubkey'])['ctext']
+        req_body['encryptedBsn'] = enc_bsn
+        response = client.post(EVENT_PATH, json=req_body)
+        assert response.status_code == 200
+        assert len(response.json) == 0
+        post = len(get_log(backend_db))
+        assert post > pre
